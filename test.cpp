e9bec7bc016f7a976278ec5cfe52357806dbb328
master <chunie0126@gmail.com>
2017-07-04 22:38:05 +0900

    std::cout practice

--- test.cpp
+++ test.cpp
@@ -18,7 +18,9 @@
 #include <iostream>
 
 int main(int argc, char* argv[]){
-  std::cout << "Hello world!" << '\n' << std::endl;
-
-  return 0;
+  std::cout << 10 << '\n' << std::endl;
+  std::cout << 10U << '\n' << std::endl;
+  std::cout << "10.5F" << '\n' << std::endl;
+  std::cout << 10.5 << '\n' << std::endl;
+  std::cout << 3 + 4 << '\n' << std::endl;
 }
