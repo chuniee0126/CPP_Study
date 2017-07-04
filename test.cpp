e5dc5184f44bb8550bf81f7f92502d3af1f1c59d
master <chunie0126@gmail.com>
2017-07-04 21:55:57 +0900

    test g++ compiler

---
+++ test.cpp
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cpp
+ *
+ *    Description:
+ *
+ *        Version:  1.0
+ *        Created:  2017년 07월 04일 21시 35분 18초
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (),
+ *   Organization:
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+
+int main(int argc, char* argv[]){
+  std::cout << "Hello world!" << '\n' << std::endl;
+
+  return 0;
+}
