c0dd503c24a5d8208afa9108ce7d8fb8089561a3
master <chunie0126@gmail.com>
2017-07-05 21:03:23 +0900

    new와 delete 를 이용한 배열형태로 동적생성과 삭제 과정 및 for문 사용법

--- test.cpp
+++ test.cpp
@@ -16,17 +16,23 @@
  * =====================================================================================
  */
 #include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[]){
-    int *pData = new int;
-    int *pNewData = new int(10);
 
-    *pData = 5;
-    std::cout << *pData << std::endl;
-    std::cout << *pNewData << std::endl;
+    // 객체를 배열 형태로 동적 생성한다.
+    int *arr = new int[5];
 
-    delete pData;
-    delete pNewData;
+    for(int i = 0; i < 5; ++i){
+        arr[i] = (i + 1) * 10;
+    }
+
+    for(int i = 0; i < 5; ++i){
+        cout << arr[i] << endl;
+    }
+
+    // 배열 형태로 생성한 대상은 반드시 배열 형태를 통해 삭제한다!
+    delete[] arr;
 
     return 0;
 }
