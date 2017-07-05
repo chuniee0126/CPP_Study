5cb5e303a084160b91aed59ce0b14eeef383e381
master <chunie0126@gmail.com>
2017-07-06 01:17:17 +0900

    범위 기반 for문 예시 코드
    참조자 사용 여부에 따른 값 변화 양상 연구

--- test.cpp
+++ test.cpp
@@ -18,28 +18,46 @@
 #include <iostream>
 using namespace std;
 
-int TestFunc(int nParam) {
-    int nResult = nParam * 2;
-
-    return nResult;
-}
-
 
 int main(int argc, char* argv[]){
-    int nInput = 0;
-    cout << "Input number: ";
-    cin >> nInput;
+    int aList[5] = {10, 20, 30, 40, 50};
 
-    // 산술 연산으로 만들어진 임시 객체에 대한 r-value 참조
-    int &&rdata = nInput + 5;
-    cout << rdata << endl;
+    // 전형적인 C 스타일 반복문
+    for(int i = 0; i < 5; ++i)
+        cout << aList[i] << ' ';
 
-    //함수 반환으로 만들어진 임시 객체에 대한 r-value 참조
-    int &&result = TestFunc(10);
+    cout << endl;
 
-    //값을 변경할 수 있다.
-    result += 10;
-    cout << result << endl;
+    // 범위 기반 C++11 스타일 반복문
+    // 각 요소의 값을 n에 복사한다.
+    for(auto n : aList){
+        cout << n << ' ';
+        n = 0;
+    }
+
+    cout << endl;
+
+    // 값에 변화가 없을 것이다.(단순 복사)
+    for(auto n : aList){
+        cout << n << ' ';
+    }
+
+    cout << endl;
+
+    // n은 각 요소에 대한 참조다.
+    for(auto &n : aList){
+        cout << n << ' ';
+        n = 0;
+    }
+
+    cout << endl;
+
+    // 값에 변화가 있을 것이다.(참조자 사용)
+    for(auto n : aList){
+        cout << n << ' ';
+    }
+
+    cout << endl;
 
     return 0;
 }
