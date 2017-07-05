f4e34c306f336ae9a09a64bf7d65feb2f3712f17
master <chunie0126@gmail.com>
2017-07-06 00:39:47 +0900

    CallByReference.cpp 참조에 의한 호출 예시 코드

--- test.cpp
+++ test.cpp
@@ -18,20 +18,15 @@
 #include <iostream>
 using namespace std;
 
+void TestFunc(int &rParam) {
+    rParam = 100;
+}
+
+
 int main(int argc, char* argv[]){
+    int nData = 0;
 
-    int nData = 10;
-
-    // nData 변수에 대한 참조자(별명) 선언
-    int &ref = nData;
-
-    // 참조자의 값을 변경하면 원본도 변경된다.
-    ref = 20;
-    cout << nData << endl;
-
-    // 포인터를 쓰는 것과 비슷하다.
-    int *pnData = &nData;
-    *pnData = 30;
+    TestFunc(nData);
     cout << nData << endl;
 
     return 0;
