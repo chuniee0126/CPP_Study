9dbb9e8cefb0ddd4a4dd7b1683d775efc498ceb7
master <chunie0126@gmail.com>
2017-07-06 01:00:46 +0900

    r-value 참조 사용법 예시 자료

--- test.cpp
+++ test.cpp
@@ -18,19 +18,28 @@
 #include <iostream>
 using namespace std;
 
-void SWAP(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
+int TestFunc(int nParam) {
+    int nResult = nParam * 2;
+
+    return nResult;
 }
 
 
 int main(int argc, char* argv[]){
-    int aData = 10;
-    int bData = 20;
+    int nInput = 0;
+    cout << "Input number: ";
+    cin >> nInput;
 
-    SWAP(aData, bData);
-    cout << aData << " is aData and "<< bData << " is bData" << "\n" << endl;
+    // 산술 연산으로 만들어진 임시 객체에 대한 r-value 참조
+    int &&rdata = nInput + 5;
+    cout << rdata << endl;
+
+    //함수 반환으로 만들어진 임시 객체에 대한 r-value 참조
+    int &&result = TestFunc(10);
+
+    //값을 변경할 수 있다.
+    result += 10;
+    cout << result << endl;
 
     return 0;
 }
