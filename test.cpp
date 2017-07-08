26f92b81b88dbcb2aa4c6536aef70b8f18135ca9
master <chunie0126@gmail.com>
2017-07-08 12:39:24 +0900

    매개변수가 두 개일 때의 디폴트 값

--- test.cpp
+++ test.cpp
@@ -18,18 +18,16 @@
 #include <iostream>
 using namespace std;
 
-
-int TestFunc(int = 10);
-
-int TestFunc(int nParam)
+// 디폴트 매개변수는 오른쪽에서 시작하며, 한번 디폴트 매개변수 선언이 시작되면, 그 다음 부터는 전부 디폴트 매개변수 선언이 이루어져야 한다.
+int TestFunc(int nParam1, int nParam2 = 2)
 {
-    return nParam;
+    return nParam1 * nParam2;
 }
 
 int main(int argc, char* argv[]){
-    cout << TestFunc() << endl;
+    cout << TestFunc(10) << endl;
 
-    cout << TestFunc(20) << endl;
+    cout << TestFunc(10, 5) << endl;
 
     return 0;
 }
