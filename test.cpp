f0a05265901fa17e74ca2a6f56b326375572af93
master <chunie0126@gmail.com>
2017-07-08 12:34:27 +0900

    Defalt parameter(디폴트 매개변수)

--- test.cpp
+++ test.cpp
@@ -19,45 +19,15 @@
 using namespace std;
 
 
+int TestFunc(int nParam = 10)
+{
+    return nParam;
+}
+
 int main(int argc, char* argv[]){
-    int aList[5] = {10, 20, 30, 40, 50};
+    cout << TestFunc() << endl;
 
-    // 전형적인 C 스타일 반복문
-    for(int i = 0; i < 5; ++i)
-        cout << aList[i] << ' ';
-
-    cout << endl;
-
-    // 범위 기반 C++11 스타일 반복문
-    // 각 요소의 값을 n에 복사한다.
-    for(auto n : aList){
-        cout << n << ' ';
-        n = 0;
-    }
-
-    cout << endl;
-
-    // 값에 변화가 없을 것이다.(단순 복사)
-    for(auto n : aList){
-        cout << n << ' ';
-    }
-
-    cout << endl;
-
-    // n은 각 요소에 대한 참조다.
-    for(auto &n : aList){
-        cout << n << ' ';
-        n = 0;
-    }
-
-    cout << endl;
-
-    // 값에 변화가 있을 것이다.(참조자 사용)
-    for(auto n : aList){
-        cout << n << ' ';
-    }
-
-    cout << endl;
+    cout << TestFunc(20) << endl;
 
     return 0;
 }
