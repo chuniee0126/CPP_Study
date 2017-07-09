91c5f982f48c149580f2dcfc2a585a42b10223cd
master <chunie0126@gmail.com>
2017-07-09 17:08:27 +0900

    클래스를 이용해 객체지향 프로그램으로 변경한 예시(C 파일과 비교할 것)

--- test.cpp
+++ test.cpp
@@ -15,25 +15,26 @@
  *
  * =====================================================================================
  */
-#include <iostream>
+#include <cstdio>
 
-using namespace std;
+class USERDATA {
 
-int nData = 200;
+public:
+    // 멤버 변수 선언
+    int nAge;
+    char szName[32];
 
-namespace Test {
-    //Test 네임스페이스 소속
-    void TestFunc(void){ cout << nData << endl; }
-
-    int nData = 100;
-} /*Test*/
-
-// 위에서 nData에 대한 선언이 이루어지지 않고 그 행이 이 장소에 오게 되면 순서대로 선언이 이루어질 때, nData가 선언되어 있지 않아 컴파일 오류가 일어난다.
-// int nData = 200;
+    // 멤버 함수 선언 및 정의
+    void Print(void) {
+        // nAge와 szName은 Print()함수의 지역 변수가 아니다!
+        printf("%d, %s\n",nAge, szName );
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    Test::TestFunc();
-
+    USERDATA user = { 10, "철수" };
+    user.Print();
+    
     return 0;
 }
