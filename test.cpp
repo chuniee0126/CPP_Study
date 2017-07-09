0353e10a0124e37d40512c3b276d3960634236a9
master <chunie0126@gmail.com>
2017-07-09 20:03:12 +0900

    멤버 함수 선언과 정의를 분리(클래스)

--- test.cpp
+++ test.cpp
@@ -32,14 +32,16 @@ public:
 
     int m_nData;
 
-    // 멤버 함수 선언 및 정의
-    void PrintData(void)
-    {
-        // 멤버 데이터에 접근하고 값을 출력한다.
-        std::cout << m_nData << std::endl;
-    }
+    // 멤버 함수 선언(선언을 clss안에서, 정의를 밖에서 진행)
+    void PrintData(void);
 
 };
+
+// 멤버 함수를 정의한다. 이 때 이 멤버함수의 소속을 명확히 구분해둔다.
+void CTest::PrintData(void){
+    // 멤버 데이터에 접근하고 값을 출력한다.
+    std::cout << m_nData << std::endl;
+}
 
 int main(int argc, char const *argv[])
 {
