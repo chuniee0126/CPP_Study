6e61b3000decc8b382792916f83c38ccc2fd49ac
master <chunie0126@gmail.com>
2017-07-09 15:21:53 +0900

    구조체와 함수 관계를 정의(C문법을 이용한 방식)

--- test.c
+++ test.c
@@ -22,18 +22,20 @@ typedef struct USERDATA
 {
     int nAge;
     char szName[32];
+    void(*Print)(struct USERDATA *);
+    // C문법에 충실하게 되면 함수를 구조체 안에 넣을 수는 없지만 19번 행처럼 객체를 초기화할 때 출력을 담당하는 함수의 주소를 기술하고 38번 행의 형태로 사용하는 것이 가능하다.
 } USERDATA;
 
 void PrintData(USERDATA *pUSER)
 {
-    printf("%d, %s\n", pUser->nAge, pUser->szName );
+    printf("%d, %s\n", pUSER->nAge, pUSER->szName );
 }
 
 // 사용자의 코드
-int int main(int argc, char const *argv[]) {
-    USERDATA user = {20, "철수"};
+int main(int argc, char const *argv[]) {
+    USERDATA user = {20, "철수", PrintData};
     // printf("%d, %s\n", User.nAge, User.szName ); // 만약 PrintData를 선언하지 않았었다면 사용자는 이 줄을 추가했어야 했을 것이다.
-    PrintData(&user);
-
+    //PrintData(&user);
+    user.Print(&user);
     return 0;
 }
