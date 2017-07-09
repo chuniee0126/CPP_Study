8ab01bed72668469e5bcf7ca4ad19e0777378850
master <chunie0126@gmail.com>
2017-07-09 19:46:45 +0900

    멤버 변수 초기화를 위한 생성자 함수 사용

--- test.cpp
+++ test.cpp
@@ -16,25 +16,34 @@
  * =====================================================================================
  */
 #include <cstdio>
+#include <iostream>
 
-class USERDATA {
+class CTest {
 
 public:
-    // 멤버 변수 선언
-    int nAge;
-    char szName[32];
+    // CTest 클래스의 '생성자 함수' 선언 및 정의
+    CTest()
+    {
+        //인스턴스가 생성되면 멤버 데이터를 자동으로 초기화
+        m_nData = 10;
+    }
+
+
+    int m_nData;
 
     // 멤버 함수 선언 및 정의
-    void Print(void) {
-        // nAge와 szName은 Print()함수의 지역 변수가 아니다!
-        printf("%d, %s\n",nAge, szName );
+    void PrintData(void)
+    {
+        // 멤버 데이터에 접근하고 값을 출력한다.
+        std::cout << m_nData << '\n';
     }
+
 };
 
 int main(int argc, char const *argv[])
 {
-    USERDATA user = { 10, "철수" };
-    user.Print();
-    
+    CTest t;
+    t.PrintData();
+
     return 0;
 }
