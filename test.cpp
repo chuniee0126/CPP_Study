3138c941e78b4de959ad4cab78336a5b62b65606
chuniee0126 <chunie0126@gmail.com>
2017-07-13 18:48:01 +0900

    객체 내부 멤버 변수의 임의 접근 차단

--- test.cpp
+++ test.cpp
@@ -18,33 +18,26 @@
 #include <cstdio>
 #include <iostream>
 
-class CTest {
+// 제작자 코드
+class CMyData {
+    // 기본 접근 제어 지시자는 'private'이다.
+    int m_nData;
+
 public:
-    // CTest 클래스의 '생성자 함수' 선언 및 정의
-    CTest() {}
+    int GetData(void) {
+        return m_nData;
+    }
 
-    // C++11부터 선언과 동시에 멤버 변수를 초기화할 수 있다!
-    int m_nData2 = 20;
-    int m_nData1 = 10;
-
-    // 멤버 함수 선언(선언을 clss안에서, 정의를 밖에서 진행)
-    void PrintData(void);
+    void SetData(int nParam) {
+        m_nData = nParam;
+    }
 };
 
-// 멤버 함수를 정의한다. 이 때 이 멤버함수의 소속을 명확히 구분해둔다.
-void CTest::PrintData(void) {
-    // 멤버 데이터에 접근하고 값을 출력한다.
-    std::cout << m_nData1 << std::endl;
-    std::cout << m_nData2 << std::endl;
-}
-
+// 사용자 코드
 int main(int argc, char const *argv[]) {
-    std::cout << "main() 함수 시작" << std::endl;
+    CMyData data;
 
-    CTest t;
-    t.PrintData();
-
-    std::cout << "main() : 함수 끝" << std::endl;
-
+    data.SetData(10);
+    std::cout << data.GetData() << std::endl;
     return 0;
 }
