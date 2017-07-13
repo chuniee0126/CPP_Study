ced68d7d33b9d7c32b640eec6f9c366ca4bf5a36
chuniee0126 <chunie0126@gmail.com>
2017-07-13 20:58:04 +0900

    new와 delete 연산자 사용

--- test.cpp
+++ test.cpp
@@ -20,10 +20,8 @@
 
 // 제작자 코드
 class CTest {
-    int m_nData;
-
 public:
-    CTest(int nParam) : m_nData(nParam) {
+    CTest() {
         std::cout << "CTest::CTest()" << std::endl;
     }
 
@@ -33,11 +31,15 @@ public:
 };
 
 int main(int argc, char const *argv[]) {
-    // CTest a; //컴파일 오류를 내보낸다 // 디폴트 생성자가 생략 되었기 때문
     std::cout << "Begin" << std::endl;
-    CTest a(10);
-    std::cout << "Before b" << std::endl;
-    CTest b(20);
+
+    // new 연산자를 이용해 동적으로 객체를 생성한다.
+    CTest *pData = new CTest;
+    std::cout << "Test" << std::endl;
+
+    // delete 연산자를 이용해 객체를 삭제한다.
+    delete pData;
     std::cout << "End" << std::endl;
+
     return 0;
 }
