145e649dc306b74277e17919c26e45d1ef1242f1
chuniee0126 <chunie0126@gmail.com>
2017-07-13 21:57:34 +0900

    디폴트 생성자의 정의를 클래스 외부로 분리

--- test.cpp
+++ test.cpp
@@ -20,42 +20,18 @@
 
 // 제작자 코드
 class CTest {
-    int m_x = 0;
-    int m_y = 0;
-
 public:
-    CTest(int x) {
-        std::cout << "CTest(int)" << std::endl;
-
-        // x값이 100이 넘는지 검사하고 넘으면 100으로 맞춘다.
-        if (x > 100) x = 100;
-
-        m_x = x;
-    }
-
-    CTest(int x, int y) : CTest(x) {
-        std::cout << "CTest(int, int)" << std::endl;
-
-        // y값이 200이 넘는지 검사하고 넘으면 200으로 맞춘다.
-        if (y > 200) y = 200;
-
-        m_y = y;
-    }
-
-    void Print() {
-        std::cout << "X :" << m_x << std::endl;
-        std::cout << "Y :" << m_y << std::endl;
-    }
+    CTest(void);
+    int m_nData = 5;
 };
 
+// 클래스 외부에서 디폴트 생성자 정의
+CTest::CTest(void) {}
+
 int main(int argc, char const *argv[]) {
-    CTest ptBegin(110);
+    CTest a;
 
-    ptBegin.Print();
-
-    CTest ptEnd(110, 250);
-
-    ptEnd.Print();
+    std::cout << a.m_nData << std::endl;
 
     return 0;
 }
