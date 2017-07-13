1e9f9a9e5f032798d0b02b069d0d92b1e2358359
chuniee0126 <chunie0126@gmail.com>
2017-07-13 21:44:39 +0900

    C++11 다른 생성자를 추가로 부르는 생성자 초기화 함수 : 생성자 위임

--- test.cpp
+++ test.cpp
@@ -20,23 +20,42 @@
 
 // 제작자 코드
 class CTest {
-    int m_nData;
+    int m_x = 0;
+    int m_y = 0;
 
 public:
-    CTest(int nParam) : m_nData(nParam) {};
-    CTest(int x, int y) : m_nData(x + y) {};
-    // 디폴트 생성자는 없다.
-    int GetData() {
-        return m_nData;
+    CTest(int x) {
+        std::cout << "CTest(int)" << std::endl;
+
+        // x값이 100이 넘는지 검사하고 넘으면 100으로 맞춘다.
+        if (x > 100) x = 100;
+
+        m_x = x;
+    }
+
+    CTest(int x, int y) : CTest(x) {
+        std::cout << "CTest(int, int)" << std::endl;
+
+        // y값이 200이 넘는지 검사하고 넘으면 200으로 맞춘다.
+        if (y > 200) y = 200;
+
+        m_y = y;
+    }
+
+    void Print() {
+        std::cout << "X :" << m_x << std::endl;
+        std::cout << "Y :" << m_y << std::endl;
     }
 };
 
 int main(int argc, char const *argv[]) {
-    CTest a(10);
-    CTest b(3, 4);
+    CTest ptBegin(110);
 
-    std::cout << a.GetData() << std::endl;
-    std::cout << b.GetData() << std::endl;
+    ptBegin.Print();
+
+    CTest ptEnd(110, 250);
+
+    ptEnd.Print();
 
     return 0;
 }
