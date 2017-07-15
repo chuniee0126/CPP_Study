c1632031a50c866e9c8ed37de3b2dc76a9b9f917
chuniee0126 <chunie0126@gmail.com>
2017-07-15 21:34:36 +0900

    서로 다른 자료형을 상용한 멤버 함수(5.5를 int로 받으면 정보손실이 일어난다)
    확인 필요 X

--- test.cpp
+++ test.cpp
@@ -18,21 +18,12 @@
 #include <cstdio>
 #include <iostream>
 
-void TestFunc(const int& nParam) {
-    // 상수형 참조였으나 일반 참조로 형변환했다.
-    int& nNewParam = const_cast<int&>(nParam);
-
-    // 따라서 l-value가 될 수 있다.
-    nNewParam = 20;
+void TestFunc(int nParam) {
+    std::cout << nParam << std::endl;
 }
 
 int main(int argc, char const *argv[]) {
-    int nData = 10;
-
-    // 상수형 참조로 전달하지만 값이 변경된다.
-    TestFunc(nData);
-
-    std::cout << nData << std::endl;
-
+    TestFunc(10);
+    TestFunc(5.5);
     return 0;
 }
