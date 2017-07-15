fc38e41d58189d513d9695db766f0aa3626a19d0
chuniee0126 <chunie0126@gmail.com>
2017-07-15 21:56:12 +0900

    멤버 함수 다중 정의 1

--- test.cpp
+++ test.cpp
@@ -18,12 +18,38 @@
 #include <cstdio>
 #include <iostream>
 
-void TestFunc(int nParam) {
-    std::cout << nParam << std::endl;
-}
+class CMyData {
+private:
+    int m_nData;
+
+public:
+    CMyData() : m_nData(0) {};
+
+    int GetData(void) {
+        return m_nData;
+    }
+
+    void SetData(int dParam) {
+        m_nData = dParam;
+    }
+
+    // 매개변수가 double 자료형인 경우로 다중 정의했다.
+    void SetData(double dParam) {
+        m_nData = 0;
+    }
+};
 
 int main(int argc, char const *argv[]) {
-    TestFunc(10);
-    TestFunc(5.5);
+    CMyData a;
+
+    // CMyData::SetData(int) 메서드가 호출된다.
+    a.SetData(10);
+    std::cout << a.GetData() << std::endl;
+
+    CMyData b;
+
+    b.SetData(5.5);
+    std::cout << b.GetData() << std::endl;
+
     return 0;
 }
