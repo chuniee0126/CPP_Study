6c10f8cdfedb6f7b814ab974381ff115cd4d72e5
chuniee0126 <chunie0126@gmail.com>
2017-07-16 14:41:09 +0900

    기본적인 단순 대입 연산자의 경우 클래스에 적용하는 경우

--- test.cpp
+++ test.cpp
@@ -31,16 +31,11 @@ public:
 
     CTest(const CTest& rhs) {
         std::cout << "CTest(const CTest &)" << std::endl;
-
-        // 새로운 메모리를 할당한다.
-        m_pnData = new int;
-
-        // 메모리 주소를 할당하는 것이 아니라, 그 안의 값을 복사하는 '깊은 복사'를 진행한다.
+        m_pnData  = new int;
         *m_pnData = *rhs.m_pnData;
     }
 
     ~CTest() {
-        // 문법적인 오류는 없지만 컴파일 해보게 되면 문제가 있음을 알 수 있다.
         Release();
     }
 
@@ -57,11 +52,11 @@ public:
 //
 int main(int argc, char const *argv[]) {
     CTest a(10);
-    // 얕은 복사로 인해 같은 포인터를 가진 친구가 멤버 변수로 들어가게 된다. 이는 메모리를 해제하는 과정에서 문제가 발생한다.
-    CTest b(a);
+    CTest b(20);
 
-    std::cout << a.GetData() << '\n';
-    std::cout << b.GetData() << std::endl;
+    // 단순 대입을 시도하면 모든 멤버의 값을 그대로 복사한다. (얕은 복사)
+    a = b;
+    std::cout << a.GetData() << std::endl;
 
     return 0;
 }
