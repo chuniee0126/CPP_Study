a4cfbe0f3ff13989ad19203c1c06a44dd22b1031
chuniee0126 <chunie0126@gmail.com>
2017-07-17 09:31:19 +0900

    연산자 다중 정의와 이동 생성자 호출 시점 std::move를 이용한 이동 생성자 명시적 이용

--- test.cpp
+++ test.cpp
@@ -18,61 +18,57 @@
 #include <cstdio>
 #include <iostream>
 
-class CTestData {
+class CMyData {
+private:
+    int m_nData = 0;
+
 public:
-    CTestData() {
-        std::cout << "CTestData():" << '\n';
+    // 변환 생성자
+    CMyData(int nParam) : m_nData(nParam) {
+        std::cout << "CMyData(int)" << '\n';
     }
 
-    ~CTestData() {
-        std::cout << "~CTestData(): " << '\n';
-    }
-
-    CTestData(const CTestData& rhs) : m_nData(rhs.m_nData) {
-        std::cout << "CTestData(const CTestData &): " << '\n';
+    // 복사 생성자
+    CMyData(const CMyData& rhs) : m_nData(rhs.m_nData) {
+        std::cout << "CMyData(const CMyData &)" << '\n';
     }
 
     // 이동 생성자
-    CTestData(CTestData&& rhs) : m_nData(rhs.m_nData) {
-        std::cout << "CTestData(CTestData &&): " << '\n';
+    CMyData(const CMyData&& rhs) : m_nData(rhs.m_nData) {
+        std::cout << "CMyData(const CMyData &&)" << '\n';
     }
 
-    // CTestData& operator=(const CTestData& rhs);
-    // {
-    // std::cout << "operator =" << '\n';
-    //
-    // m_nData = rhs.m_nData;
-    //
-    // return *this;
-    // }
-
-    int GetData() {
+    // 형변환
+    operator int() {
         return m_nData;
     }
 
-    void SetData(int nParam) {
-        m_nData = nParam;
+    // +
+    CMyData operator+(const CMyData& rhs) {
+        std::cout << "operator+" << '\n';
+        CMyData result(0);
+        result.m_nData = this->m_nData + rhs.m_nData;
+
+        return std::move(result);
     }
 
-private:
-    int m_nData = 0;
+    CMyData& operator=(const CMyData& rhs) {
+        std::cout << "operator=" << '\n';
+        m_nData = rhs.m_nData;
+
+        return *this;
+    }
 };
 
-
-CTestData TestFunc(int nParam) {
-    std::cout << "**TestFunc(): Begin***" << '\n';
-    CTestData a;
-    a.SetData(nParam);
-    std::cout << "**TestFunc(): End***" << '\n';
-
-    return a;
-}
-
 int main(int argc, char const *argv[]) {
-    std::cout << "******Before******" << '\n';
-    CTestData b = std::move(TestFunc(20));
-    std::cout << "******After******" << '\n';
-    // CTestData c(b);
+    std::cout << "*********Begin**********" << '\n';
+    CMyData a(0), b(3), c(4);
+
+    // b+c 연산을 실행하면 이름 없는 임시 객체가 만들어지며
+    // a에 대입하는 것은 이 임시 객체다.
+    a = b + c;
+    std::cout << a << '\n';
+    std::cout << "*********End**********" << '\n';
 
     return 0;
 }
