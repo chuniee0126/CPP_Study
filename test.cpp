349090de6c36513ab91bc676bf41369241edbe9b
chuniee0126 <chunie0126@gmail.com>
2017-07-19 09:45:25 +0900

    이동 대입 연산자

--- test.cpp
+++ test.cpp
@@ -24,15 +24,27 @@ private:
 
 public:
     explicit CMyData(int nParam) {
+        std::cout << "CMyData(int)" << '\n';
         m_pnData = new int (nParam);
+    }
+
+    CMyData(const CMyData& rhs) {
+        m_pnData = new int (*rhs.m_pnData);
     }
 
     ~CMyData() {
         delete m_pnData;
     }
 
+    CMyData operator+(const CMyData& rhs) {
+        // 호출자 함수에서 이름 없는 임시 객체가 생성된다.
+        return std::move(CMyData(*m_pnData + *rhs.m_pnData));
+    }
+
     // 단순 대입 연산자 다중 정의
     CMyData& operator=(const CMyData& rhs) {
+        std::cout << "operator=" << '\n';
+
         // r-value가 자신이면 대입을 수행하지 않는다.
         if (this == &rhs) return *this;
 
@@ -41,6 +53,16 @@ public:
 
         // 새로 할당한 메모리에 값을 저장한다.
         m_pnData = new int (*rhs.m_pnData);
+
+        return *this;
+    }
+
+    CMyData& operator=(CMyData&& rhs) {
+        std::cout << "operator=(move)" << '\n';
+
+        // 얕은 복사를 수행하고 원본은  nullptr로 포기화한다.
+        m_pnData     = rhs.m_pnData;
+        rhs.m_pnData = nullptr;
 
         return *this;
     }
@@ -64,10 +86,14 @@ public:
 };
 
 int main(int argc, char const *argv[]) {
-    CMyData a(0), b(5), c(10);
+    CMyData a(0), b(3), c(4);
 
-    a += b;
-    a += c;
+    std::cout << "*****before*****" << '\n';
+
+    a = b + c;
+    std::cout << "*****After*****" << '\n';
+    std::cout << a << '\n';
+    a = b;
     std::cout << a << '\n';
 
     return 0;
